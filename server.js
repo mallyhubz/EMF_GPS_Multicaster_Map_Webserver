d98f9c2b3e5804ff863cad5a0860c37bd118b1d5
Hubert <43018466+mallyhubz@users.noreply.github.com>
2026-06-29 22:56:28 +0100

    Tidy up html pages

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(fs.readFileSync("index.html"));
     }
-	if (req.url === "/emfmap.html") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(fs.readFileSync("emfmap.html"));
-    }
 });
 
 // ------------------------------
